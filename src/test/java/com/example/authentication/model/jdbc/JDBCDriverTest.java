c14cd9a88cc1bc686f05ae8ef629429cf6d1b04f
Robert Kensen <robert.kensen@outlook.com>
2021-05-10 13:33:35 +0200

    Initialized database connection test

---
+++ src/test/java/com/example/authentication/model/jdbc/JDBCDriverTest.java
@@ -0,0 +1,13 @@
+package com.example.authentication.model.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Robert Kensen on 10-5-2021
+ * @project Authentication
+ */
+
+@SpringBootTest
+public class JDBCDriverTest {
+}
